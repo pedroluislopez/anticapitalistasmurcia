bcec99bb658ce72e04f31ca49b983bd77583e9c0
Pedro Luis López Sánchez <plopez@neuromobilemarketing.com>
2016-04-24 11:59:44 +0200

    Delete env and set requirements

--- env/lib/python3.4/site-packages/reversion/signals.py
+++
@@ -1,6 +0,0 @@
-from django.dispatch.dispatcher import Signal
-
-
-# Version management signals.
-pre_revision_commit = Signal(providing_args=["instances", "revision", "versions"])
-post_revision_commit = Signal(providing_args=["instances", "revision", "versions"])
